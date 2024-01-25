820ab44b8393b2497caaa690e832f42ac7002c03
Chels93 <148895873+Chels93@users.noreply.github.com>
2024-01-25 17:29:36 -0500

    ammended scripts.js
    corrected syntax errors to make 'add' button usable and 'X' delete button usable

--- scripts.js
+++ scripts.js
@@ -1,39 +1,43 @@
-function newItem() {
 
-    let li = $('<li></li>');
+// 1. Adding a new item to the list of items:
+function newItem() {
     let inputValue = $('#input').val();
-    li.append(inputValue);
+
 
     if (inputValue === '') {
-        alert("you must write something");
+        alert("You must write something!");
     } else {
+
+        let crossOutButton = $('<button>X</button>');
+        crossOutButton.on("click", deleteListItem);
+
+        let li = $('<li></li>').text(inputValue);
+        li.append(crossOutButton);
         $('#list').append(li);
     }
 
-    //2. Crossing out an item from the list of items:
-    function crossOut() {
-        li.toggleClass("strike");
+    // 2. Crossing out an item from the list of items:
+    function deleteListItem() {
+        $(this).toggleClass("strike");
+
     }
 
-    li.on("dblclick", function crossOut() {
-        li.toggleClass("strike");
+    $(document).on("dblclick", "li", crossOut);
 
-        //3(i). Adding the delete button "X": 
-        let crossOutButton = $('<crossOutButton</><crossOutButton>');
-        crossOutButton.append(document.createTextNode('X'));
-        li.append(crossOutButton);
+    // 3(i). Adding the delete button "X": 
+    let crossOutButton = $('<button>X</button>');
+    crossOutButton.on("click", deleteListItem);
+    crossOutButton.on("click", crossOutButton);
 
-        crossOutButton.on("click", deleteListItem);
-        function deleteListItem() {
-            li.addClass("delete")
-        }
+    $('#list').append(crossOutButton);
 
-        //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-        function deleteListItem() {
-            li.classList.add("delete")
-        }
+    // 3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+    function deleteListItem() {
+        $(this).parent().addClass("delete");
+    }
 
-        // 4. Reordering the items: 
-        $('#list').sortable();
+    // 4. Reordering the items: 
+    $('#list').sortable();
 
+}
     }
